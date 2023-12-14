68fd6f2dbd8e88def3d8d88406b4df003ee0b52f
Jesse Bloom <jbloom@fredhutch.org>
2023-12-14 11:22:25 -0500

    test titers are as expected (#9)
    * test titers are as expected
    
    * format and lint
    
    * only test values close, not exact
    
    * re-run pipeline to get current expected titers

---
+++ test_example/test_titers_as_expected.py
@@ -0,0 +1,30 @@
+"""Test script to test titers as expected."""
+
+
+import numpy
+
+import pandas as pd
+
+
+expected_titers = pd.read_csv("expected_titers_for_test.csv").assign(
+    log10_nt50=lambda x: numpy.log10(x["nt50"])
+)[["serum", "virus", "log10_nt50", "n_replicates", "nt50_bound"]]
+
+actual_titers = pd.read_csv("results/aggregated_titers/titers.csv").assign(
+    log10_nt50=lambda x: numpy.log10(x["nt50"])
+)[["serum", "virus", "log10_nt50", "n_replicates", "nt50_bound"]]
+
+rtol = 0.02
+atol = 0.1
+
+print(f"Comparing {expected_titers} and {actual_titers} with {atol=} and {rtol=}")
+
+pd.testing.assert_frame_equal(
+    expected_titers,
+    actual_titers,
+    check_exact=False,
+    atol=atol,
+    rtol=rtol,
+)
+
+print("Titers are sufficiently similar.")
