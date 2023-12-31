4c06193ba4fbb28989b06ebc0869081a36e16987
jbloom <jbloom@fredhutch.org>
2023-12-30 17:22:18 -0800

    update expected titers for test

--- test_example/test_titers_as_expected.py
+++ test_example/test_titers_as_expected.py
@@ -7,12 +7,12 @@ import pandas as pd
 
 
 expected_titers = pd.read_csv("expected_titers_for_test.csv").assign(
-    log10_nt50=lambda x: numpy.log10(x["nt50"])
-)[["serum", "virus", "log10_nt50", "n_replicates", "nt50_bound"]]
+    log10_titer=lambda x: numpy.log10(x["titer"])
+)[["serum", "virus", "log10_titer", "n_replicates", "titer_bound"]]
 
 actual_titers = pd.read_csv("results/aggregated_titers/titers.csv").assign(
-    log10_nt50=lambda x: numpy.log10(x["nt50"])
-)[["serum", "virus", "log10_nt50", "n_replicates", "nt50_bound"]]
+    log10_titer=lambda x: numpy.log10(x["titer"])
+)[["serum", "virus", "log10_titer", "n_replicates", "titer_bound"]]
 
 rtol = 0.02
 atol = 0.1
